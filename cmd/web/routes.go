b92ee4f96a6c118c470f5da6ebca614713a8856d
Leonardo de Faveri <faver_i@hotmail.com>
2024-03-17 20:16:31 -0300

    remove unused var

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func routes(app *config.AppConfig) http.Handler {
+func routes(_ *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -21,10 +21,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handler.Repo.Generals)
 	mux.Get("/majors-suite", handler.Repo.Majors)
 
-	mux.Get("/search-availability",handler.Repo.Availability)
+	mux.Get("/search-availability", handler.Repo.Availability)
 	mux.Post("/search-availability", handler.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handler.Repo.AvailabilityJSON)
-	
+
 	mux.Get("/contact", handler.Repo.Contact)
 
 	mux.Get("/make-reservation", handler.Repo.Reservation)
