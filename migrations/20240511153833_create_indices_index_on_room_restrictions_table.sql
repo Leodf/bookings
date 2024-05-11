7e13ae0d2b574c9c0c79afad0535ec10025963c5
Leonardo de Faveri <faver_i@hotmail.com>
2024-05-11 14:00:48 -0300

    refactor wrong drop index to right

--- migrations/20240511153833_create_indices_index_on_room_restrictions_table.sql
+++ migrations/20240511153833_create_indices_index_on_room_restrictions_table.sql
@@ -4,6 +4,6 @@ CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_room_id_idx ON room_restrict
 CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_reservation_id_idx ON room_restrictions(reservation_id);
 
 -- +goose Down
-DROP INDEX IF EXISTS users_email_idx;
+DROP INDEX IF EXISTS room_restrictions_start_date_end_date_idx;
 DROP INDEX IF EXISTS room_restrictions_room_id_idx;
 DROP INDEX IF EXISTS room_restrictions_reservation_id_idx;
