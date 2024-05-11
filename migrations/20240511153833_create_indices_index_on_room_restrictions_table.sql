07a95a685a1fe07a8871b57ad5671377e78fb99a
Leonardo de Faveri <faver_i@hotmail.com>
2024-05-11 13:15:44 -0300

    create indices on room_restrictions table migration

---
+++ migrations/20240511153833_create_indices_index_on_room_restrictions_table.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_start_date_end_date_idx ON room_restrictions(start_date, end_date);
+CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_room_id_idx ON room_restrictions(room_id);
+CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_reservation_id_idx ON room_restrictions(reservation_id);
+
+-- +goose Down
+DROP INDEX IF EXISTS users_email_idx;
+DROP INDEX IF EXISTS room_restrictions_room_id_idx;
+DROP INDEX IF EXISTS room_restrictions_reservation_id_idx;
