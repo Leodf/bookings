db46aaa6e8bf3298b1cdd989cfa2556243fa433e
Leonardo de Faveri <faver_i@hotmail.com>
2024-05-11 12:22:30 -0300

    alter room_restrictions table migration

---
+++ migrations/20240511151253_alter_room_restrictions_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+ALTER TABLE room_restrictions
+ADD CONSTRAINT room_restrictions_rooms_id_fk
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE room_restrictions
+ADD CONSTRAINT room_restrictions_restrictions_id_fk
+FOREIGN KEY (restriction_id) REFERENCES restrictions(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- +goose Down
+ALTER TABLE room_restrictions
+DROP CONSTRAINT room_restrictions_rooms_id_fk;
+
+ALTER TABLE room_restrictions
+DROP CONSTRAINT room_restrictions_restrictions_id_fk;
+
