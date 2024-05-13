c41fb08ad2a39f4e2642b9ea43be10f89bbf421d
Leonardo de Faveri <faver_i@hotmail.com>
2024-05-13 19:05:20 -0300

    create driver package to access database

---
+++ internal/driver/driver.go
@@ -0,0 +1,57 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+	// _ "github.com/jackc/pgx/v5"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
+
+// ConnectSQL creates database pool for PostgreSQL
+func ConnectSQL(dns string) (*DB, error) {
+	d, err := NewDatabase(dns)
+	if err != nil {
+		panic(err)
+	}
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+	err = testDB(d)
+	if err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+// testDB tries to ping the database
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase creates a new database for the application
+func NewDatabase(dns string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dns)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
