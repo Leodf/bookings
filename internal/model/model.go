5527e26185be7be7043c7f129ec6f7ef311945b8
Leonardo de Faveri <faver_i@hotmail.com>
2024-05-16 20:53:45 -0300

    add models to app

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,10 @@
 package model
 
-import "github.com.br/Leodf/bookings/internal/forms"
+import (
+	"time"
+
+	"github.com.br/Leodf/bookings/internal/forms"
+)
 
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
@@ -21,4 +25,62 @@ type Reservation struct {
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users is the user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the Rooms model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the Restrictions model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the Reservations model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the RoomRestrictions model
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
