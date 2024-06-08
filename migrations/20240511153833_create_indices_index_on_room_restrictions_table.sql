b95b9fa8ce10f19b2e569d3f4ab8e4e57e253a04
Leonardo de Faveri <faver_i@hotmail.com>
2024-06-08 17:10:51 -0300

    refactor unnecessary unique indices on migrations

--- migrations/20240511153833_create_indices_index_on_room_restrictions_table.sql
+++ migrations/20240511153833_create_indices_index_on_room_restrictions_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
-CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_start_date_end_date_idx ON room_restrictions(start_date, end_date);
-CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_room_id_idx ON room_restrictions(room_id);
-CREATE UNIQUE INDEX IF NOT EXISTS room_restrictions_reservation_id_idx ON room_restrictions(reservation_id);
+CREATE INDEX IF NOT EXISTS room_restrictions_start_date_end_date_idx ON room_restrictions(start_date, end_date);
+CREATE INDEX IF NOT EXISTS room_restrictions_room_id_idx ON room_restrictions(room_id);
+CREATE INDEX IF NOT EXISTS room_restrictions_reservation_id_idx ON room_restrictions(reservation_id);
 
 -- +goose Down
 DROP INDEX IF EXISTS room_restrictions_start_date_end_date_idx;
--- migrations/20240511170243_create_indices_on_reservations_table.sql
+++ migrations/20240511170243_create_indices_on_reservations_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
-CREATE UNIQUE INDEX IF NOT EXISTS reservations_email_idx ON reservations(email);
-CREATE UNIQUE INDEX IF NOT EXISTS reservations_last_name_idx ON reservations(last_name);
+CREATE INDEX IF NOT EXISTS reservations_email_idx ON reservations(email);
+CREATE INDEX IF NOT EXISTS reservations_last_name_idx ON reservations(last_name);
 
 -- +goose Down
 DROP INDEX IF EXISTS reservations_email_idx;
